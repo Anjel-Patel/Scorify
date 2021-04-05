9f2c09caa0e479ead3663ed3a887a964cc68c478
Anjel-Patel <anjelpatel54@gmail.com>
2021-04-05 20:40:42 +0530

    Refined TopBar and implemented variable font weights

--- scorify/src/TopBar.js
+++ scorify/src/TopBar.js
@@ -1,5 +1,7 @@
-import TopBarCSS from './TopBar.css';
-import {ReactComponent as Logo} from './assets/scorifyLogo.svg';
+import './TopBar.css';
+import Logo from './assets/scorifyLogo.png';
+import Profilephoto from './assets/userpng.png'
+import {ReactComponent as LogoSVG} from './assets/scorifyLogo.svg';
 import './index.css';
 
 function TopBar()
@@ -8,8 +10,9 @@ function TopBar()
         <div className="bar-rect">
             <div className="side">
                 <div className="logo-with-text">
-                    <Logo className="logo"/>
-                    <h4 className="h4 scorify">scorify.</h4>
+                    <LogoSVG className="logo"/>
+                    {/* <img src={Logo} className="logopng"/> */}
+                    <h4 className="h4 scorify">scorify</h4>
                 </div>
                 <h4 className="h4 link dashboard">Dashboard</h4>
                 <h4 className="h4 link">History</h4>
@@ -19,7 +22,9 @@ function TopBar()
             <div className="side">
                 <h4 className="h4 link">About</h4>
                 <h4 className="h4 link">Contact</h4>
-                <div className="profile"></div>
+                <div className="profile">
+                    <img className="profile-photo" src={Profilephoto} alt="😎"/>
+                </div>
             </div>
         </div>
     );
