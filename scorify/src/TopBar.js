ae032f6a179bdc812b73af93b14f65118ad43104
Anjel-Patel <anjelpatel54@gmail.com>
2021-04-05 16:53:59 +0530

    frontend - Added styles and a Top Navigation Bar(TopBar)

---
+++ scorify/src/TopBar.js
@@ -0,0 +1,28 @@
+import TopBarCSS from './TopBar.css';
+import {ReactComponent as Logo} from './scorifyLogo.svg';
+import './index.css';
+
+function TopBar()
+{
+    return(
+        <div className="bar-rect">
+            <div className="side">
+                <div className="logo-with-text">
+                    <Logo className="logo"/>
+                    <h4 className="h4 scorify">scorify.</h4>
+                </div>
+                <h4 className="h4 link dashboard">Dashboard</h4>
+                <h4 className="h4 link">History</h4>
+                <h4 className="h4 link">Leaderboard</h4>
+            </div>
+            <div className="empty-div"></div>
+            <div className="side">
+                <h4 className="h4 link">About</h4>
+                <h4 className="h4 link">Contact</h4>
+                <div className="profile"></div>
+            </div>
+        </div>
+    );
+}
+
+export default TopBar;
