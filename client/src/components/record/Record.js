f33bffa37f419ebac4e8ba6c2edfcbd61717f44b
gedia147 <parthgedia8@gmail.com>
2021-04-17 13:26:24 +0530

    attendance solved

--- client/src/components/record/Record.js
+++ client/src/components/record/Record.js
@@ -1,27 +1,54 @@
 import "./Record.css";
 import RecordRow from  "./RecordRow";
-import {useState} from "react";
+import Axios from "axios";
+import { useState, useEffect } from 'react';
 import {ReactComponent as InfoSVG} from "../../assets/info.svg";
 
-function somefunc(detailsState){
-    console.log(detailsState[0].name);
-}
 
-function Rowmaker(detailsState, setDetails) {
+// function somefunc(detailsState){
+//     console.log(detailsState[0].name);
+// }
+
+// function Rowmaker(detailsState, setDetails) {
+//     //Loops through the details array
+//     return detailsState.map((info, i) => (
+//         <RecordRow name= {info.name}
+//         key={i}
+//         role= {info.role}
+//         prev_score={info.prev_score} 
+//         prev_normalHours={info.prev_normalHours} 
+//         prev_overtimeHours={info.prev_overtimeHours} 
+//         attendance={info.attendance} 
+//         overtimeHours={(info.overtimeHours>=0 && info.attendance===1)?info.overtimeHours:"-"}
+//         setDetails={setDetails}/>
+//     ));
+// }
+//     // {  console.log(xyz && curDate);
+//     console.log(xyz[curDate][1003]['attendance']);
+//     console.log(curDate);
+// }
+
+function Rowmaker(currentDetails,xyz,setxyz,curDate) {
+    // console.log(curDate);
     //Loops through the details array
-    return detailsState.map((info, i) => (
-        <RecordRow name= {info.name}
+    if( typeof(xyz[curDate]) !== 'undefined')
+    {return currentDetails.map((info, i) => (
+        
+        <RecordRow name= {info.fullname}
         key={i}
-        role= {info.role}
-        prev_score={info.prev_score} 
-        prev_normalHours={info.prev_normalHours} 
-        prev_overtimeHours={info.prev_overtimeHours} 
-        attendance={info.attendance} 
-        overtimeHours={(info.overtimeHours>=0 && info.attendance===1)?info.overtimeHours:"-"}
-        setDetails={setDetails}/>
-    ));
-}
-
+        details = {xyz}
+        date = {curDate}
+        empID={info.empID}
+        role= {'Member'}
+        prev_score={info.score} 
+        prev_normalHours={info.normalhours} 
+        prev_overtimeHours={info.overtime} 
+        attendance={xyz[curDate][info.empID]['attendance']} 
+        overtimeHours={xyz[curDate][info.empID]['overtimeHours']}
+        setDetails={setxyz}
+        />
+    ));}
+};
 function SatRowmaker(details) {
     return details.map((info, i) => (
         <div className="sat-details-wrapper" style={{marginBottom:'12px'}}>
@@ -31,6 +58,46 @@ function SatRowmaker(details) {
 }
 function Record() {
 
+    const [currentDetails, setCurrentDetails] = useState([]);
+    const [dates,setDates] = useState([]);
+    const [curDate,setCurDate] = useState(''); 
+    const [xyz,setxyz] = useState({});
+    var dict ={};
+    useEffect(() => {
+        Axios.get("http://localhost:8000/currentrecords").then((response) => {
+            const [res1,res2]=(response.data).split("   ",2);
+            // console.log(response.data);
+            const cd = JSON.parse(res1);
+            setCurrentDetails(cd);
+            const d =JSON.parse(res2);
+            setDates(d);
+            // console.log(JSON.parse(res2)[0]);
+            setCurDate(d[0]);
+            
+                for(let i=0;i<7;i++)
+                {   if(i!==4){//nosunday
+                    var e = {};
+                    for(let j=0;j<cd.length;j++)
+                        e[cd[j].empID] ={ attendance: 1, overtimeHours : '-'};
+                    dict[d[i]]=e;
+                    }
+                };
+                // console.log(dict);
+            setxyz(dict);
+            
+          });
+        },[]);
+
+    
+    
+    
+        // setxyz(dict);
+// console.log(dict); 
+
+    
+    
+    
+    
     const details = [
         {name : 'Kelvin Gupta', role : 'Member', prev_score: 400, prev_normalHours: 30, prev_overtimeHours: 10, overtimeHours: 2, attendance: 1, satisfaction: 7},
         {name : 'Jonas Sharma', role : 'Member', prev_score: 375, prev_normalHours: 28, prev_overtimeHours: 10, overtimeHours: 4, attendance: 0, satisfaction: 9},
@@ -39,10 +106,10 @@ function Record() {
         {name : 'Raju Sebastain', role : 'Member', prev_score: 250, prev_normalHours: 20, prev_overtimeHours: 5, overtimeHours: -1, attendance: 0, satisfaction: 6},
     ];
 
-    const [detailsState, setDetails] = useState(details);
+    // const [detailsState, setDetails] = useState(details);
     const current_week = '21/04/2021';
 
-    const date_list = ['21/04/2021','22/04/2021','23/04/2021','24/04/2021','25/04/2021','26/04/2021'];
+    // const date_list = ['21/04/2021','22/04/2021','23/04/2021','24/04/2021','25/04/2021','26/04/2021'];
 
     return(
         <div className="page-rect">
@@ -61,14 +128,16 @@ function Record() {
 
                     {/* Main Header */}
                     <div className="main-header-wrapper">
-                        <select classname="h5 date-dropdown" name="date-dropdown" id="date-dropdown" onChange={(e)=>{console.log(e)}}>
+                        <select className="h5 date-dropdown" name="date-dropdown" id="date-dropdown" onChange={(e)=>{setCurDate(e.target.value)}}>
                             <optgroup className="optgroup h5">
-                                <option value={date_list[0]}>{date_list[0]}</option>
-                                <option value={date_list[1]}>{date_list[1]}</option>
-                                <option value={date_list[2]}>{date_list[2]}</option>
-                                <option value={date_list[3]}>{date_list[3]}</option>
-                                <option value={date_list[4]}>{date_list[4]}</option>
-                                <option value={date_list[5]}>{date_list[5]}</option>
+                                <option value={dates[0]}>{dates[0]}</option>
+                                <option value={dates[1]}>{dates[1]}</option>
+                                <option value={dates[2]}>{dates[2]}</option>
+                                <option value={dates[3]}>{dates[3]}</option>
+                                {/* <option value={dates[4]}>{dates[4]}</option> */}
+                                <option value={dates[5]}>{dates[5]}</option>
+                                <option value={dates[6]}>{dates[6]}</option>
+
                             </optgroup>
                         </select>
                         <h4 className="h4" style={{color:'var(--neutral-400)', marginLeft:'302px'}}>Performance last week</h4>
@@ -88,15 +157,15 @@ function Record() {
                     </div>
 
                     {/* Rows */}
-                    <div className="rows">{Rowmaker(detailsState, setDetails)}</div>
+                    <div className="rows">{Rowmaker(currentDetails,xyz,setxyz,curDate)}</div>
                     <div className="empty-div"></div>
 
                     {/* SAVE BUTTON */}
                     <div className="record-save-btn">
-                        <h4 className="h4 record-save-text" style={{userSelect:'none'}} onClick={somefunc(detailsState)}>Save</h4>
+                        <h4 className="h4 record-save-text" style={{userSelect:'none'}} >Save</h4>
                     </div>
                 </div>
-
+                {/* onClick={somefunc(detailsState)} */}
                 {/* RIGHT SIDE */}
                 <div className="sat-wrapper">
                     <h4 className="h4" style={{marginTop:'24px'}}>Current Week</h4>
--- client/src/components/record/RecordRow.js
+++ client/src/components/record/RecordRow.js
@@ -1,10 +1,34 @@
+import Attendance from "../dashboard/Attendance";
 import "./RecordRow.css";
 
-function attendanceLogger(value, key, setDetails) {
-    console.log(key);
-}
+function keepCloning(objectpassed) {
+    if (objectpassed === null || typeof objectpassed !== 'object') {
+       return objectpassed;
+    }
+  // give temporary-storage the original obj's constructor
+  var temporarystorage = objectpassed.constructor(); 
+    for (var key in objectpassed) {
+      temporarystorage[key] = keepCloning(objectpassed[key]);
+    }
+    return temporarystorage;
+  };
 
-function RecordRow({name, role, prev_score, prev_normalHours, prev_overtimeHours, attendance, overtimeHours, key, setDetails}) {
+function attendanceLogger(value, details,date,empID, setDetails) {
+    
+    let temp= keepCloning(details);
+    if(value===0){
+        temp[date][empID]['attendance']=1;
+        setDetails(temp);
+    }
+    else
+    {
+        temp[date][empID]['attendance']=0;
+        setDetails(temp);
+    }
+
+};
+
+function RecordRow({name,details,date,empID, role, prev_score, prev_normalHours, prev_overtimeHours, attendance, overtimeHours, setDetails}) {
     return(
         <div className="row-rect " style={{width:'1214px', color:'var(--neutral-800)', marginTop:'0px', marginBottom:'12px'}}>
             <h5 className="p1 leader-board-details" style={{left : '24px'}}>{name}</h5>
@@ -14,10 +38,10 @@ function RecordRow({name, role, prev_score, prev_normalHours, prev_overtimeHours
             <h5 className="p1 leader-board-details" style={{left : '777px', color:'var(--neutral-500)'}}>{prev_score}</h5>
             <h5 className="p1 leader-board-details" style={{left : '973px'}}>{overtimeHours}</h5>
             <div className="record-attendance-rect" style={{left : '1088px'}}>
-                <div id="present" className={"record-attendance-present-rect" + (attendance===1?" present-selected":"")} onClick={attendanceLogger(1, key, setDetails)}>
+                <div id="present" className={"record-attendance-present-rect" + (attendance===1?" present-selected":"" )} onClick={(e)=>{attendanceLogger(attendance, details,date,empID, setDetails)}} >
                     <h5 style={{userSelect:'none'}}>P</h5>
                 </div>
-                <div id="absent" className={"record-attendance-absent-rect" + (attendance===0?" absent-selected":"")} onClick={attendanceLogger(0, key, setDetails)}>
+                <div id="absent" className={"record-attendance-absent-rect" + (attendance===0?" absent-selected":"")} onClick={(e)=>{attendanceLogger(attendance, details,date,empID, setDetails)}}>
                     <h5 style={{userSelect:'none'}}>A</h5>
                 </div>
             </div>
